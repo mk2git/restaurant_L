c63709188083ff9dacb66f1eea3e708fd43f7187
mk2git <86880555+mk2git@users.noreply.github.com>
2024-04-18 14:38:41 +0900

    重複して同じテーブルが表示されていたのを修正

--- resources/views/checkouts/index.blade.php
+++ resources/views/checkouts/index.blade.php
@@ -6,20 +6,28 @@
       <div class="row bg-white">
         <div class="col-10 d-flex justify-content-between flex-wrap">
           @foreach ($Atables as $Atable)
+            @php
+                $found = false;
+            @endphp
             @foreach ($checkoutTables as $checkoutTable)
               @if ($Atable->id == $checkoutTable->table_id && $checkoutTable->check_status == 'not yet')
+                @php
+                    $found = true;
+                @endphp
                 <button type="button" class="btn select-Atable" data-bs-toggle="modal" data-bs-target="#selectCheckTableAModal{{$Atable->id}}" data-Atable-id="{{ $Atable->id }}">
                   <div class="m-3 check-table">
                       <x-Atable-style :atable-name="$Atable->name" />
                     </div>
                 </button>
-                <x-modal-select-check-Atable :atable-id="$Atable->id" :atable-name="$Atable->name" />
-              @else
-                <div class="m-3">
-                  <x-Atable-style :atable-name="$Atable->name" />
-                </div>
+                <x-modal-select-check-Atable :atable-id="$Atable->id" :atable-name="$Atable->name" /> 
+                @break
               @endif
             @endforeach
+            @if (!$found)
+              <div class="m-3">
+                <x-Atable-style :atable-name="$Atable->name" />
+              </div>
+            @endif
           @endforeach
         </div>
         <div class="col-2">
@@ -34,25 +42,33 @@
       <div class="row bg-white">
         <div class="col-10 d-flex justify-content-between flex-wrap">
           @foreach ($Btables as $Btable)
+            @php
+                $found = false;
+            @endphp
             @foreach ($checkoutTables as $checkoutTable)
               @if ($Btable->id == $checkoutTable->table_id && $checkoutTable->check_status == 'not yet')
+                @php
+                    $found = true;
+                @endphp
                 <button type="button" class="btn select-Btable" data-bs-toggle="modal" data-bs-target="#selectCheckTableAModal{{$Btable->id}}" data-Btable-id="{{ $Btable->id }}">
                   <div class="m-3 check-table">
                       <x-Btable-style :btable-name="$Btable->name" />
                     </div>
                 </button>
                 <x-modal-select-check-Btable :btable-id="$Btable->id" :btable-name="$Btable->name" />
-              @else
-                <div class="m-3">
-                  <x-Btable-style :btable-name="$Btable->name" />
-                </div>
+                @break
               @endif
             @endforeach
+            @if (!$found)
+              <div class="m-3">
+                <x-Btable-style :btable-name="$Btable->name" />
+              </div>
+            @endif
           @endforeach
         </div>
         <div class="col-2 border d-flex align-items-center justify-content-center">
           <span>厨房</span>
-      </div>
+        </div>
       </div>
     </div>
   </div>
