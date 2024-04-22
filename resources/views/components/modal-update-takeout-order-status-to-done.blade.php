21b412d3b2ef38a00b2479368c537b13131163f6
mk2git <86880555+mk2git@users.noreply.github.com>
2024-04-22 12:37:12 +0900

    表示コメントの修正

--- resources/views/components/modal-update-takeout-order-status-to-done.blade.php
+++ resources/views/components/modal-update-takeout-order-status-to-done.blade.php
@@ -2,17 +2,17 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleTakeoutModalLabel"><i class="fa-solid fa-bell-concierge"></i>&nbsp;&nbsp;配膳</h1>
+        <h1 class="modal-title fs-5" id="exampleTakeoutModalLabel"><i class="fa-solid fa-bell-concierge"></i>&nbsp;&nbsp;調理確認</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body text-center">
-       <p>{{$takeoutOrderMenuName}}を配膳しましたか？</p> 
+       <p>{{$takeoutOrderMenuName}}は完成しましたか？</p> 
        <div>
          <form action="{{route('takeout-order.updateStatusDone', $takeoutOrderId)}}" method="post">
             @csrf
             @method('put')
             <input type="hidden" name="takeout_order_id" value="{{$takeoutOrderId}}">
-            <button type="submit" class="btn btn-success">配膳済み</button>
+            <button type="submit" class="btn btn-success">調理済み</button>
         </form>
        </div>
       </div>
