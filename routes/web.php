e28b278b4276e258223cf88ee4059d8bbeaabbfd
mk2git <86880555+mk2git@users.noreply.github.com>
2024-04-22 18:01:23 +0900

    showアクションのルートを追加

--- routes/web.php
+++ routes/web.php
@@ -153,6 +153,7 @@ Route::middleware('auth')->group(function () {
     Route::controller(TakeoutCheckoutController::class)->group(function(){
         Route::get('checkout/takeout', 'index')->name('takeout-check.index');
         Route::post('serve', 'storeAndUpdate')->name('takeout-check.store');
+        Route::get('checkout/takeout/{takeout_id}', 'show')->name('takeout-check.show');
     });
 
 });
