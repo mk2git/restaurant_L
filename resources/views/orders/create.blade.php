4bb998331ef960bb1a3f030b6e665cb1ae40a5bf
mk2git <86880555+mk2git@users.noreply.github.com>
2024-04-24 14:49:16 +0900

    数量をselectタグで選択し、毎回アイコンを押さないといけなかったのをなくすよう修正

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -3,19 +3,48 @@
     <p class="h3">テーブル名：{{$table->name}}</p>
   </div>
 
-  {{-- エラーメッセージ --}}
-  @if ($errors->any())
-    <x-error-message />
-  @endif
-
-  @if (session('message'))
-    <x-alert-message :type="session('type')" :message="session('message')" />
-  @endif
-
-  <div class="container-menu mt-3 rounded w-75 mx-auto p-5 mb-5">
+  <div class="container-menu mt-3 rounded w-50 mx-auto p-5 mb-5">
     <p class="text-center h2"><i class="fa-solid fa-utensils"></i>&nbsp;&nbsp;メニュー</p>
 
-    @foreach ($categories as $category)
+    {{-- <form action="{{route('orders.store', $table->id)}}" method="post">
+       @csrf --}}
+       <form action="{{route('orders.store')}}" method="post">
+        @csrf
+        @foreach ($categories as $category)
+          <h5 class="text-start fw-bold ps-5">{{$category->name}}</h5>
+          <hr class="w-25">
+          <ul>
+            @foreach ($menus as $menu)
+              @if($category->id == $menu->category_id)
+              <li class="mb-2">
+                <div class="row">
+                  <div class="col-3">
+                    <p>{{$menu->name}}</p>
+                  </div>
+                  <div class="col-2">・・・・・</div>
+                  <div class="col-3">
+                    <span>{{number_format($menu->price)}}円</span>
+                  </div>
+                  <div class="col-4">
+                      <input type="hidden" name="menu_id" value="{{$menu->id}}">
+                      <select name="quantity[{{$menu->id}}]" class="form-control">
+                        @for ($i=0; $i <= 10 ; $i++)
+                          <option value="{{$i}}">{{$i}}</option>
+                        @endfor
+                      </select>
+                  </div>
+                </div>
+              </li>
+              @endif
+            @endforeach
+          </ul>
+        @endforeach
+        <input type="hidden" name="table_id" value="{{$table->id}}">
+        <div class="text-center mt-5">
+           <button type="submit" class="btn btn-success w-25">注文確認へ</button>
+        </div>
+      </form>
+    {{-- @foreach ($categories as $category)
       <h5 class="text-start fw-bold ps-5">{{$category->name}}</h5>
       <hr class="w-25">
       <ul>
@@ -31,8 +60,7 @@
                 <span>{{number_format($menu->price)}}円</span>
               </div>
               <div class="col-2">
-                <form action="{{route('orders.store', $table->id)}}" method="post">
-                  @csrf
+                
                   <input type="hidden" name="menu_id" value="{{$menu->id}}">
                   <input type="hidden" name="table_id" value="{{$table->id}}">
                   <select name="quantity" class="form-control">
@@ -51,10 +79,10 @@
           @endif
         @endforeach
       </ul>
-    @endforeach
+    @endforeach --}}
   </div>
 
-  <div class="w-50 mx-auto mb-5 text-center">
+  {{-- <div class="w-50 mx-auto mb-5 text-center">
     <a href="{{route('orders.edit', $table->id)}}" class="btn btn-success w-50">注文確認</a>
-  </div>
+  </div> --}}
 </x-app-layout>
