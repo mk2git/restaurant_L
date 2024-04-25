2c34ee45d18e3ae5cde4b38a3c570ba7a4edc691
mk2git <86880555+mk2git@users.noreply.github.com>
2024-04-25 17:51:19 +0900

    会計後再度同じテーブルの注文を受け付けた際、過去に同じテーブルで注文されたものが表示されないよう修正

--- app/Http/Controllers/ServeController.php
+++ app/Http/Controllers/ServeController.php
@@ -18,7 +18,7 @@ class ServeController extends Controller
     {
         $table_ids = Order::where('check_status', 'not yet')->distinct()->get('table_id');
         $order_tables = Table::whereIn('id', $table_ids)->select('id', 'name')->get();
-        $orders = Order::all();
+        $orders = Order::where('check_status', 'not yet')->get();
 
         $takeout_order_ids = Takeout_Order::where('check_status', 'not yet')->whereDate('created_at', today())->distinct()->get('takeout_id');
         $takeout_order_names = Takeout::whereIn('id', $takeout_order_ids)->select('id', 'name')->get();
