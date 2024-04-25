ffe7ebf93c8a333cb22441db3cee0ff13ad0d61e
mk2git <86880555+mk2git@users.noreply.github.com>
2024-04-25 16:44:39 +0900

    closeボタンを押して閉じるよう修正

--- resources/views/components/modal-select-order-Atable.blade.php
+++ resources/views/components/modal-select-order-Atable.blade.php
@@ -15,7 +15,7 @@
       <div class="modal-content">
           <div class="modal-header">
               <h5 class="modal-title" id="selectTableAModalLabel"><i class="fa-solid fa-utensils"></i> &nbsp;&nbsp;注文</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
               </button>
           </div>
