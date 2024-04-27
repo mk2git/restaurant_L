83379219992a21255b598fee5570bb326bb9ce15
mk2git <86880555+mk2git@users.noreply.github.com>
2024-04-27 18:17:45 +0900

    トランザクションの追加

--- app/Http/Controllers/TakeoutOrderController.php
+++ app/Http/Controllers/TakeoutOrderController.php
@@ -7,6 +7,7 @@ use App\Models\Takeout;
 use App\Models\Menu;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TakeoutOrderController extends Controller
 {
@@ -27,24 +28,30 @@ class TakeoutOrderController extends Controller
      */
     public function store(Request $request)
     {
-        $data = $request->all();
-
-         // メニューごとに注文を処理
-        foreach ($data['quantity'] as $menuId => $quantity) {
-        // quantityがnullまたは0の場合はスキップ
-        if ($quantity === null || $quantity == 0) {
-            continue;
+        try{
+            DB::beginTransaction();
+            $data = $request->all();
+            // メニューごとに注文を処理
+            foreach ($data['quantity'] as $menuId => $quantity) {
+            // quantityがnullまたは0の場合はスキップ
+            if ($quantity === null || $quantity == 0) {
+                continue;
+            }
+            // 注文を作成して保存
+            $takeoutOrder = new Takeout_Order();
+            $takeoutOrder->takeout_id = $data['takeout_id'];
+            $takeoutOrder->menu_id = $menuId;
+            $takeoutOrder->quantity = $quantity;
+            $takeoutOrder->save();
         }
-
-        // 注文を作成して保存
-        $takeoutOrder = new Takeout_Order();
-        $takeoutOrder->takeout_id = $data['takeout_id'];
-        $takeoutOrder->menu_id = $menuId;
-        $takeoutOrder->quantity = $quantity;
-        $takeoutOrder->save();
-    }
-
-        return redirect()->route('takeout-order.edit', ['takeout_id' => $request->takeout_id]);
+            DB::commit();
+            return redirect()->route('takeout-order.edit', ['takeout_id' => $request->takeout_id]);
+            
+        }catch(\Throwable $th){
+            DB::rollBack();
+            logger('Error Category Store', ['message' => $th->getMessage()]);
+            return redirect()->back()->with('error', 'カテゴリー追加に失敗しました');
+        }
     }
 
     /**
@@ -64,27 +71,40 @@ class TakeoutOrderController extends Controller
      */
     public function update($takeout_order_id, Request $request)
     {
-        // dd($request);
-        $takeout_order = Takeout_Order::find($takeout_order_id);
-        $takeout_order->quantity = $request->input('quantity');
-        $takeout_order->save();
+        try{
+            DB::beginTransaction();
+            $takeout_order = Takeout_Order::find($takeout_order_id);
+            $takeout_order->quantity = $request->input('quantity');
+            $takeout_order->save();
+            DB::commit();
+            $menu_name = Menu::find($takeout_order->menu_id)->name;
+            $message = '「'.$menu_name.'」注文数が変更されました';
+            $takeout_id = $request->takeout_id;
+            return redirect()->route('takeout-order.edit', ['takeout_id' => $takeout_id])->with(['message' => $message, 'type' => 'success']);
 
-        $menu_name = Menu::find($takeout_order->menu_id)->name;
-        $message = '「'.$menu_name.'」注文数が変更されました';
-        $takeout_id = $request->takeout_id;
-        // dd($takeout_id);
-
-        return redirect()->route('takeout-order.edit', ['takeout_id' => $takeout_id])->with(['message' => $message, 'type' => 'success']);
+        }catch(\Throwable $th){
+            DB::rollBack();
+            logger('Error Category Store', ['message' => $th->getMessage()]);
+            return redirect()->back()->with('error', 'カテゴリー追加に失敗しました');
+        }
     }
 
-    public function updateStatusDone(Request $request){
+    public function updateStatusDone(Request $request)
+    {
+        try{
+            DB::beginTransaction();
+            $takeout_order = Takeout_Order::find($request->takeout_order_id);
+            $takeout_order->status = config('takeout_order.done');
+            $takeout_order->save();
+            DB::commit();
+            $message = 'テイクアウト「'.$takeout_order->menu->name.'」の準備が完了しました';
+            return redirect()->route('serve.index', ['takeout_id' => $takeout_order->takeout_id])->with(['message' => $message, 'type' => 'success']);
 
-        $takeout_order = Takeout_Order::find($request->takeout_order_id);
-        $takeout_order->status = 'done';
-        $takeout_order->save();
-
-        $message = 'テイクアウト「'.$takeout_order->menu->name.'」の準備が完了しました';
-        return redirect()->route('serve.index', ['takeout_id' => $takeout_order->takeout_id])->with(['message' => $message, 'type' => 'success']);
+        }catch(\Throwable $th){
+            DB::rollBack();
+            logger('Error TakeoutOrder updateStatusDone', ['message' => $th->getMessage()]);
+            return redirect()->back()->with('error', 'takeout_ordersテーブルのstatusの更新に失敗しました');
+        }
     }
 
     /**
@@ -92,15 +112,22 @@ class TakeoutOrderController extends Controller
      */
     public function destroy($takeout_order_id, Request $request)
     {
-        $takeout_order = Takeout_Order::find($takeout_order_id);
-        $menu_name = Menu::find($takeout_order->menu_id)->name;
-        $message = '「'.$menu_name.'」が削除されました';
-        $takeout_id = $request->takeout_id;
+        try{
+            DB::beginTransaction();
+            $takeout_order = Takeout_Order::find($takeout_order_id);
+            $menu_name = Menu::find($takeout_order->menu_id)->name;
+            $message = '「'.$menu_name.'」が削除されました';
+            $takeout_id = $request->takeout_id;
 
-        $takeout_order->delete();
+            $takeout_order->delete();
+            DB::commit();            
+            return redirect()->route('takeout-order.edit', ['takeout_id' => $takeout_id])->with(['message' => $message, 'type' => 'danger']);
 
-        
-        return redirect()->route('takeout-order.edit', ['takeout_id' => $takeout_id])->with(['message' => $message, 'type' => 'danger']);
+        }catch(\Throwable $th){
+            DB::rollBack();
+            logger('Error TakeoutOrder Destroy', ['message' => $th->getMessage()]);
+            return redirect()->back()->with('error', '注文料理の削除に失敗しました');
+        }
     }
 
     public function sendMessage(){
