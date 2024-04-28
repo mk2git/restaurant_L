814b280ebd6e73137c44db24872305df5c9f628b
mk2git <86880555+mk2git@users.noreply.github.com>
2024-04-28 17:11:07 +0900

    不要な箇所の削除

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -5,28 +5,6 @@
         </h2>
     </x-slot>
 
-    {{-- <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
-        </div>
-    </div> --}}
-
     <div class="container m-5">
         @if (session('message'))
           <x-alert-message :type="session('type')" :message="session('message')" />
@@ -60,8 +38,7 @@
                                   </select>
                               </td>
                               <td>
-                                    <input type="hidden" name="id" value="{{$user->id}}">
-                                    <button type="submit" class="btn btn-success w-50">変更</button>
+                                 <button type="submit" class="btn btn-success w-50">変更</button>
                                 </form>
                               </td>
                           </tr>
