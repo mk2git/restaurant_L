0eb3afe3099be1c92462431416d99f1c8838e9ef
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-01 11:29:42 +0900

    へルパ関数の修正

--- app/Helpers/OrderHelper.php
+++ app/Helpers/OrderHelper.php
@@ -3,24 +3,28 @@
   use Carbon\Carbon;
 
 
-   if(!function_exists('getLastMonthOrders')){
-      function getLastMonthOrders(){
-        $startDateLastMonth = Carbon::now()->subMonth()->startOfMonth();
-        $endDateLastMonth = Carbon::now()->subMonth()->endOfMonth();
-
-        return Order::whereBetween('created_at', [$startDateLastMonth, $endDateLastMonth])->get();
+   if(!function_exists('getLastMonthStartDay')){
+      function getLastMonthStartDay(){      
+        return $startDateLastMonth = Carbon::now()->subMonth()->startOfMonth();
       }
    }
+   if(!function_exists('getLastMonthAndDay')){
+    function getLastMonthEndDay(){
+      return $endDateLastMonth = Carbon::now()->subMonth()->endOfMonth();
+    }
+ }
 
-   if(!function_exists('getThisMonthOrders')){
-      function getThisMonthOrders()
+   if(!function_exists('getThisMonthStartDay')){
+      function getThisMonthStartDay()
         {
-            $startDate = Carbon::now()->startOfMonth();
-            $endDate = Carbon::now()->endOfMonth();
-
-            return Order::whereBetween('created_at', [$startDate, $endDate])->get();
+           return $startDate = Carbon::now()->startOfMonth();
         }
    }
-   
+   if(!function_exists('getThisMonthEndDay')){
+    function getThisMonthEndDay()
+      {
+         return $endDate = Carbon::now()->endOfMonth();
+      }
+ }
    
     
