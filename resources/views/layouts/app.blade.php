f1fbf8a7c259e856a02d4a184650ebfa9aad75c2
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-01 21:07:58 +0900

    componentから@includeに修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,8 +16,8 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         {{-- jQueryのスクリプト --}}
-        {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> --}}
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        {{-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script> --}}
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
 
@@ -33,7 +33,7 @@
 
     </head>
     <body class="font-sans antialiased @yield('background-class')">
-        <x-navigation />
+        @include('layouts.navigation')
 
         <!-- Page Heading -->
         @if (isset($header))
