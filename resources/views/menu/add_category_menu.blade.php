543c7de3ec3d54d43fb1d6470cb4968c09456efc
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-02 11:30:21 +0900

    componentの利用に修正

--- resources/views/menu/add_category_menu.blade.php
+++ resources/views/menu/add_category_menu.blade.php
@@ -49,53 +49,7 @@
                     <div class="card-body">
                         <form action="{{route('menu.store')}}" method="post" enctype="multipart/form-data">
                             @csrf
-                            <div class="form-group row mb-3">
-                                <div class="col-sm-3">
-                                    <label for="name" class="col-form-label">料理名：</label>
-                                </div>
-                                <div class="col-sm-9">
-                                    <input type="text" name="name" id="name" class="form-control">
-                                </div>
-                            </div>
-                            <div class="form-group row mb-3">
-                                <div class="col-sm-3">
-                                    <label for="price" class="col-form-label">価格：</label>
-                                </div>
-                                <div class="col-sm-9">
-                                    <input type="number" name="price" id="price" min="0" class="form-control">
-                                </div>
-                            </div>
-                            <div class="form-group row mb-3">
-                                <div class="col-sm-3">
-                                    <label class="col-form-label">カテゴリー</label>
-                                </div>
-                                <div class="col-sm-9">
-                                    <select name="category_id" class="form-control">
-                                        <option value="" selected disabled>選択してください</option>
-                                        @foreach ($categories as $category)
-                                        <option value="{{$category->id}}">{{$category->name}}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-
-                            <div class="form-group row mb-3">
-                                <div class="col-sm-3">
-                                    <label for="description" class="col-form-label">説明：</label>
-                                </div>
-                                <div class="col-sm-9">
-                                    <textarea name="description" id="description" cols="20" rows="5" class="form-control"></textarea>
-                                </div>
-                            </div>
-                            <div class="form-group row mb-3">
-                                <div class="col-sm-3">
-                                    <label for="photo" class="col-form-label">写真：</label>
-                                </div>
-                                <div class="col-sm-9">
-                                    <input type="file" name="photo" id="photo" class="form-control-file">
-                                </div>
-                            </div>
-                            <button type="submit" class="btn btn-success w-50">追加</button>
+                            <x-menu-form :categories="$categories" />
                         </form>
                     </div>
                 </div>
