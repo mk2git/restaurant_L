d175e615c502bf5ab8ce13e043a524a5495dfc10
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-02 12:26:45 +0900

    注文確認のformをcomponentに追加

---
+++ resources/views/components/confirm-order.blade.php
@@ -0,0 +1,69 @@
+<ul class="my-5">
+  @if (isset($orders))
+      @foreach ($orders as $order)
+        <li class="mb-3">
+          <div class="row">
+            <div class="col-3">
+              {{$order->menu->name}}
+            </div>
+            <div class="col-3">・・・・・</div>
+            <div class="col-2">
+                <form action="{{route('orders.update', $order->id)}}" method="post">
+                @csrf
+                @method('put')
+                <select name="quantity" class="form-control">
+                  @for ($i=1; $i <= 10; $i++)
+                    <option value="{{$i}}" @if($order->quantity == $i) selected @endif>{{$i}}</option>
+                  @endfor
+                </select>
+            </div>
+            <div class="col-2 d-flex justify-content-center">
+              <button type="submit" class=""><i class="fa-solid fa-arrows-rotate text-success"></i></button>
+              </form>
+            </div>
+            <div class="col-2">
+                <form action="{{route('orders.destroy', $order->id)}}" method="post">
+                  @csrf
+                  @method('delete')
+                  <input type="hidden" name="table_id" value="{{$order->table_id}}">       
+                  <button type="submit" class="btn w-50"><i class="fa-regular fa-trash-can text-danger"></i></button>
+                </form>
+            </div>
+          </div>
+        </li>
+      @endforeach
+  @else
+    @foreach ($takeoutOrders as $order)
+      <li class="mb-3">
+        <div class="row">
+          <div class="col-3">
+            {{$order->menu->name}}
+          </div>
+          <div class="col-3">・・・・・</div>
+          <div class="col-2">
+              <form action="{{route('takeout-order.update', $order->id)}}" method="post">
+              @csrf
+              @method('put')
+              <select name="quantity" class="form-control">
+                @for ($i=1; $i <= 10; $i++)
+                  <option value="{{$i}}" @if($order->quantity == $i) selected @endif>{{$i}}</option>
+                @endfor
+              </select>
+          </div>
+          <div class="col-2 d-flex justify-content-center">
+            <button type="submit" class=""><i class="fa-solid fa-arrows-rotate text-success"></i></button>
+            </form>
+          </div>
+          <div class="col-2">
+              <form action="{{route('takeout-order.delete', $order->id)}}" method="post">
+                @csrf
+                @method('delete')
+                <input type="hidden" name="takeout_id" value="{{$order->takeout_id}}">          
+                <button type="submit" class="btn w-50"><i class="fa-regular fa-trash-can text-danger"></i></button>
+              </form>
+          </div>
+        </div>
+      </li>
+    @endforeach
+  @endif
+</ul>
