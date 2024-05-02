142e6115ad449d81ea1ba6b1bb4f818b5e494f50
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-02 12:27:09 +0900

    componentの利用に修正

--- resources/views/orders/edit.blade.php
+++ resources/views/orders/edit.blade.php
@@ -3,46 +3,11 @@
     <x-alert-message :type="session('type')" :message="session('message')" />
   @endif
 
-  <div class="container mt-5 w-75">
+  <div class="container mt-5 w-50">
     <h2 class=""><i class="fa-solid fa-utensils"></i>&nbsp;&nbsp;注文確認</h2>
     <hr>
-    <ul class="my-5">
-      @foreach ($orders as $order)
-        <li class="mb-3">
-          <div class="row">
-            <div class="col-3">
-              {{$order->menu->name}}
-            </div>
-            <div class="col-3">・・・・・</div>
-            <div class="col-2">
-              <form action="{{route('orders.update', $order->id)}}" method="post">
-                @csrf
-                @method('put')
-                <select name="quantity" class="form-control">
-                  @for ($i=1; $i <= 10; $i++)
-                    <option value="{{$i}}" @if($order->quantity == $i) selected @endif>{{$i}}</option>
-                  @endfor
-                </select>
-            </div>
-            <div class="col-2">
-              <button type="submit" class="btn btn-success w-50">更新</button>
-              </form>
-            </div>
-            <div class="col-2">
-              <form action="{{route('orders.destroy', $order->id)}}" method="post">
-                @csrf
-                @method('delete')
-                <input type="hidden" name="table_id" value="{{$order->table_id}}">
-                <button type="submit" class="btn w-50"><i class="fa-regular fa-trash-can text-danger"></i></button>
-              </form>
-            </div>
-          </div>
-        </li>
-      @endforeach
-    </ul>
-
+    <x-confirm-order :orders="$orders" />
     <hr>
-
     <div class="mt-5 text-center">
         <form action="{{route('orders.changeOrderStatus')}}" method="post">
           @csrf
