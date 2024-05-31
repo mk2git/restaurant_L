1006553a913a633c446219144b47bf00755a8125
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-31 11:15:58 +0900

    不要なコード削除とマージン調整

--- resources/views/components/modal-select-menu-img.blade.php
+++ resources/views/components/modal-select-menu-img.blade.php
@@ -1,15 +1,3 @@
-{{-- <script>
-  $(document).ready(function() {
-      $('.menu-img').click(function() {
-          var menuId = $(this).data('menu-id');
-          $.get('/order/create/' + menuId, function(data) {
-              $('#menuModal' + menuId).find('.modal-body').html(data);
-              $('#menuModal' + menuId).modal('show');
-          });
-      });
-  });
-</script> --}}
-
 <div class="modal fade" id="menuModal{{$menuId}}" tabindex="-1" role="dialog" aria-labelledby="menuModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -21,7 +9,7 @@
       </div>
       <div class="modal-body text-center">
         <img src="{{$menuImg}}" alt="イメージ写真" class="img-fluid mx-auto">
-        <p>＜料理の説明＞</p>
+        <p class="mt-3">＜料理の説明＞</p>
         <p>{{$menuDesc}}</p>
       </div>
       <div class="modal-footer">
