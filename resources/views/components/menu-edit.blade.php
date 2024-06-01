dcfe3d2ffb1c19c703bd0618a61afb58b5d5b601
mk2git <86880555+mk2git@users.noreply.github.com>
2024-06-01 22:24:28 +0900

    btnクラス追加

--- resources/views/components/menu-edit.blade.php
+++ resources/views/components/menu-edit.blade.php
@@ -11,13 +11,13 @@
           <span>{{number_format($menuPrice)}}円</span>
       </div>
       <div class="col-2">
-          <button type="button" class="edit-menu" data-toggle="modal" data-target="#editMenuModal{{$menuId}}" data-menu-id="{{ $menuId }}">
+          <button type="button" class="btn edit-menu" data-toggle="modal" data-target="#editMenuModal{{$menuId}}" data-menu-id="{{ $menuId }}">
               <i class="fa-solid fa-pencil"></i>
           </button>
           <x-modal-edit-menu :menu-id="$menuId" :menu-name="$menuName" :menu-price="$menuPrice" :menu-description="$menuDescription" :menu-photo="$menuPhoto" :menu-category-id="$menuCategoryId" :categories="$categories" />
       </div>
       <div class="col-2">
-          <button type="button" data-toggle="modal" data-target="#deleteMenuModal{{$menuId}}" data-menu-id="{{ $menuId }}" class="delete-menu">
+          <button type="button" data-toggle="modal" data-target="#deleteMenuModal{{$menuId}}" data-menu-id="{{ $menuId }}" class="btn delete-menu">
               <i class="fa-regular fa-trash-can text-danger"></i>
           </button>
           <x-modal-delete-menu :menu-id="$menuId" :menu-name="$menuName" />
