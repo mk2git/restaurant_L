c0718d2f2810dfbc48c84957b4a3da55d6f33955
mk2git <86880555+mk2git@users.noreply.github.com>
2024-06-01 18:15:45 +0900

    closeボタンの修正

--- resources/views/components/modal-unused-Btable.blade.php
+++ resources/views/components/modal-unused-Btable.blade.php
@@ -4,9 +4,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="unusedTableBModalLabel">座席</h5>
-        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <p class="text-center">{{ $seatType }}-{{$seatNumber}}を使用しますか？</p>
