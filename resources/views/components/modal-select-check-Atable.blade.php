7643dbfbd32e8e59e4e2abbaf95b4bce331b3862
mk2git <86880555+mk2git@users.noreply.github.com>
2024-06-01 18:15:32 +0900

    不要なコード削除

--- resources/views/components/modal-select-check-Atable.blade.php
+++ resources/views/components/modal-select-check-Atable.blade.php
@@ -1,15 +1,3 @@
-<script>
-  $(document).ready(function() {
-      $('.select-Atable').click(function() {
-          var tableId = $(this).data('table-id');
-          $.get('/order/' + tableId, function(data) {
-              $('#selectCheckTableAModal' + tableId).find('.modal-body').html(data);
-              $('#selectCheckTableAModal' + tableId).modal('show');
-          });
-      });
-  });
-</script>
-
 <div class="modal fade" id="selectCheckTableAModal{{$tableId}}" tabindex="-1" role="dialog" aria-labelledby="selectCheckTableAModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
       <div class="modal-content">
--- resources/views/components/modal-select-check-Btable.blade.php
+++ resources/views/components/modal-select-check-Btable.blade.php
@@ -1,15 +1,3 @@
-<script>
-  $(document).ready(function() {
-      $('.select-Btable').click(function() {
-          var tableId = $(this).data('table-id');
-          $.get('/order/' + tableId, function(data) {
-              $('#selectCheckTableBModal' + tableId).find('.modal-body').html(data);
-              $('#selectCheckTableBModal' + tableId).modal('show');
-          });
-      });
-  });
-</script>
-
 <div class="modal fade" id="selectCheckTableBModal{{$tableId}}" tabindex="-1" role="dialog" aria-labelledby="selectCheckTablebModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
       <div class="modal-content">
