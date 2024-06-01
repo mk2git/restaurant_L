a02fe2b053ae095bab2f32525b368e0af05b3fb9
mk2git <86880555+mk2git@users.noreply.github.com>
2024-06-01 22:21:07 +0900

    successに修正

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -17,9 +17,9 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
+            <x-success-button>
                 {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+            </x-success-button>
         </div>
     </form>
 </x-guest-layout>
