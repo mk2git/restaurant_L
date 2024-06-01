d40295f59908799c186b7164002c8393d1b1b82f
mk2git <86880555+mk2git@users.noreply.github.com>
2024-06-01 23:40:00 +0900

    モーダルが動くよう修正

--- resources/views/components/menu-edit.blade.php
+++ resources/views/components/menu-edit.blade.php
@@ -11,15 +11,18 @@
           <span>{{number_format($menuPrice)}}円</span>
       </div>
       <div class="col-2">
-          <button type="button" class="btn edit-menu" data-toggle="modal" data-target="#editMenuModal{{$menuId}}" data-menu-id="{{ $menuId }}">
-              <i class="fa-solid fa-pencil"></i>
-          </button>
-          <x-modal-edit-menu :menu-id="$menuId" :menu-name="$menuName" :menu-price="$menuPrice" :menu-description="$menuDescription" :menu-photo="$menuPhoto" :menu-category-id="$menuCategoryId" :categories="$categories" />
+        <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editMenuModal{{$menuId}}">
+            <i class="fa-solid fa-pencil"></i>
+        </button>
+        <x-modal-edit-menu :menu-id="$menuId" :menu-name="$menuName" :menu-price="$menuPrice" :menu-description="$menuDescription" :menu-photo="$menuPhoto" :menu-category-id="$menuCategoryId" :categories="$categories" />
       </div>
       <div class="col-2">
-          <button type="button" data-toggle="modal" data-target="#deleteMenuModal{{$menuId}}" data-menu-id="{{ $menuId }}" class="btn delete-menu">
+        <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#deleteMenuModal{{$menuId}}">
+            <i class="fa-regular fa-trash-can text-danger"></i>
+        </button>
+          {{-- <button type="button" data-toggle="modal" data-target="#deleteMenuModal{{$menuId}}" data-menu-id="{{ $menuId }}" class="btn delete-menu">
               <i class="fa-regular fa-trash-can text-danger"></i>
-          </button>
+          </button> --}}
           <x-modal-delete-menu :menu-id="$menuId" :menu-name="$menuName" />
       </div>
   </div>
