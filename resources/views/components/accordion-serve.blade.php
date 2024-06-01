8d840f54ec10b154651f75bc6ed938a47941cb16
mk2git <86880555+mk2git@users.noreply.github.com>
2024-06-01 22:38:20 +0900

    classを追加

--- resources/views/components/accordion-serve.blade.php
+++ resources/views/components/accordion-serve.blade.php
@@ -9,14 +9,14 @@
       <div class="accordion-body">
         @foreach ($orders as $order)
           @if ($order->table_id == $orderTableId)
-            <div class="row mb-3">
+            <div class="row mb-1">
                 <div class="col-sm-7">
                   <small @if($order->status == config('order.done')) class="text-decoration-line-through" @endif>{{$order->menu->name}}</small>
                 </div>
                 <div class="col-sm-2">×&nbsp;{{$order->quantity}}</div>
                 <div class="col-sm-3">
                   @if ($order->status == config('order.cooking'))
-                    <button type="submit" data-bs-toggle="modal" data-bs-target="#exampleModal{{$order->id}}"><i class="fa-solid fa-bell-concierge text-success"></i></button>
+                    <button type="submit" data-bs-toggle="modal" data-bs-target="#exampleModal{{$order->id}}" class="btn"><i class="fa-solid fa-bell-concierge text-success"></i></button>
                     <x-modal-update-order-status-to-done :order-id="$order->id" :order-menu-name="$order->menu->name" />
                   @else
                     <i class="fa-solid fa-check text-danger"></i>
