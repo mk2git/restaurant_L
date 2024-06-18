a0a2dc221ed26f6bec7b3eefaa5b0a9926306085
mk2git <86880555+mk2git@users.noreply.github.com>
2024-06-18 17:17:37 +0900

    キャンセル用のアイコンとモーダルの追加

--- resources/views/components/accordion-serve.blade.php
+++ resources/views/components/accordion-serve.blade.php
@@ -10,14 +10,26 @@
         @foreach ($orders as $order)
           @if ($order->table_id == $orderTableId)
             <div class="row mb-1">
-                <div class="col-sm-7">
+                <div class="col-6">
                   <small @if($order->status == config('order.done')) class="text-decoration-line-through" @endif>{{$order->menu->name}}</small>
                 </div>
-                <div class="col-sm-2">×&nbsp;{{$order->quantity}}</div>
-                <div class="col-sm-3">
+                <div class="col-2">×&nbsp;{{$order->quantity}}</div>
+                <div class="col-4">
                   @if ($order->status == config('order.cooking'))
-                    <button type="submit" data-bs-toggle="modal" data-bs-target="#exampleModal{{$order->id}}" class="btn"><i class="fa-solid fa-bell-concierge text-success"></i></button>
-                    <x-modal-update-order-status-to-done :order-id="$order->id" :order-menu-name="$order->menu->name" />
+                    <div class="row">
+                      <div class="col">
+                        <button type="submit" data-bs-toggle="modal" data-bs-target="#exampleModal{{$order->id}}" class="btn">
+                          <small><i class="fa-solid fa-bell-concierge text-success"></i></small>
+                        </button>
+                        <x-modal-update-order-status-to-done :order-id="$order->id" :order-menu-name="$order->menu->name" />
+                      </div>
+                      <div class="col">
+                        <button type="submit" data-bs-toggle="modal" data-bs-target="#cancelModal{{$order->id}}" class="btn">
+                          <small><i class="fa-solid fa-ban text-danger"></i></small>
+                        </button>
+                         <x-modal-cancel-order :order-id="$order->id" :order-menu-name="$order->menu->name" />
+                      </div>
+                    </div>                        
                   @else
                     <i class="fa-solid fa-check text-danger"></i>
                   @endif
