128122eec8fc2157df6fadf878114c85269f0723
mk2git <86880555+mk2git@users.noreply.github.com>
2024-06-19 23:54:56 +0900

    paddingの調整、アイコンごとに配膳とキャンセルのテキスト追加、テキストのフォントサイズ修正

--- resources/views/components/accordion-serve.blade.php
+++ resources/views/components/accordion-serve.blade.php
@@ -6,26 +6,36 @@
       </button>
     </h2>
     <div id="panelsStayOpen-collapseOne{{$orderTableId}}" class="accordion-collapse collapse show">
-      <div class="accordion-body">
+      <div class="accordion-body px-3">
         @foreach ($orders as $order)
           @if ($order->table_id == $orderTableId)
-            <div class="row mb-1">
+            <div class="row mb-1 text-center">
                 <div class="col-6">
                   <small @if($order->status == config('order.done')) class="text-decoration-line-through" @endif>{{$order->menu->name}}</small>
                 </div>
-                <div class="col-2">×&nbsp;{{$order->quantity}}</div>
+                <div class="col-2">
+                  <small>×&nbsp;{{$order->quantity}}</small>
+                </div>
                 <div class="col-4">
                   @if ($order->status == config('order.cooking'))
                     <div class="row">
                       <div class="col">
-                        <button type="submit" data-bs-toggle="modal" data-bs-target="#exampleModal{{$order->id}}" class="btn">
-                          <small><i class="fa-solid fa-bell-concierge text-success"></i></small>
+                        <button type="submit" data-bs-toggle="modal" data-bs-target="#exampleModal{{$order->id}}" class="btn p-0">
+                          <small>
+                            <i class="fa-solid fa-bell-concierge text-success">
+                            <small class="d-block">配膳</small>
+                          </i>
+                          </small>
                         </button>
                         <x-modal-update-order-status-to-done :order-id="$order->id" :order-menu-name="$order->menu->name" />
                       </div>
                       <div class="col">
-                        <button type="submit" data-bs-toggle="modal" data-bs-target="#cancelModal{{$order->id}}" class="btn">
-                          <small><i class="fa-solid fa-ban text-danger"></i></small>
+                        <button type="submit" data-bs-toggle="modal" data-bs-target="#cancelModal{{$order->id}}" class="btn p-0">
+                          <small>
+                            <i class="fa-solid fa-ban text-danger">
+                              <small class="d-block">キャンセル</small>
+                            </i>
+                          </small>
                         </button>
                          <x-modal-cancel-order :order-id="$order->id" :order-menu-name="$order->menu->name" />
                       </div>
