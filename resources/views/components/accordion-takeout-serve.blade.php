8056b60c3614270ccac560f4129cdd335cfdcc0f
mk2git <86880555+mk2git@users.noreply.github.com>
2024-06-20 11:37:03 +0900

    accordion内のデザイン修正

--- resources/views/components/accordion-takeout-serve.blade.php
+++ resources/views/components/accordion-takeout-serve.blade.php
@@ -2,29 +2,36 @@
   <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpenTakeOut-collapseOne{{$takeoutOrderId}}" aria-expanded="true" aria-controls="panelsStayOpenTakeOut-collapseOne{{$takeoutOrderId}}">
-        {{$takeoutOrderName}}
+        {{$takeoutOrderName}}&nbsp;<small>様</small>
       </button>
     </h2>
     <div id="panelsStayOpenTakeOut-collapseOne{{$takeoutOrderId}}" class="accordion-collapse collapse show" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">
+      <div class="accordion-body px-3">
         @foreach ($takeoutOrders as $takeoutOrder)
           @if ($takeoutOrder->takeout_id == $takeoutOrderId)     
-            <div class="row mb-3">
-                <div class="col-sm-7">
+            <div class="row mb-1 text-center">
+                <div class="col-sm-6">
                   <small @if($takeoutOrder->status == config('takeout_order.done')) class="text-decoration-line-through" @endif>{{$takeoutOrder->menu->name}}</small>
                 </div>
                 <div class="col-sm-2">
                   <small>×&nbsp;{{$takeoutOrder->quantity}}</small>
                 </div>
-                <div class="col-sm-3">
+                <div class="col-sm-4">
                   @if ($takeoutOrder->status == config('takeout_order.cooking'))
-                    <button type="submit" data-bs-toggle="modal" data-bs-target="#exampleTakeoutModal{{$takeoutOrder->id}}" class="btn"><i class="fa-solid fa-bell-concierge text-success"></i></button>
-                    <x-modal-update-takeout-order-status-to-done :takeout-order-id="$takeoutOrder->id" :takeout-order-menu-name="$takeoutOrder->menu->name" />
-                      
-                    <button type="submit" data-bs-toggle="modal" data-bs-target="#cancelTakeoutModal{{$takeoutOrder->id}}" class="btn">
-                      <small><i class="fa-solid fa-ban text-danger"></i></small>
-                    </button>
-                    <x-modal-cancel-takeout :takeout-order-id="$takeoutOrder->id" :takeout-order-menu-name="$takeoutOrder->menu->name" />
+                  <div class="row">
+                    <div class="col">
+                      <button type="submit" data-bs-toggle="modal" data-bs-target="#exampleTakeoutModal{{$takeoutOrder->id}}" class="btn p-0">
+                        <i class="fa-solid fa-bell-concierge text-success"><small class="d-block">配膳</small></i>
+                      </button>
+                      <x-modal-update-takeout-order-status-to-done :takeout-order-id="$takeoutOrder->id" :takeout-order-menu-name="$takeoutOrder->menu->name" />
+                    </div>
+                    <div class="col">
+                      <button type="submit" data-bs-toggle="modal" data-bs-target="#cancelTakeoutModal{{$takeoutOrder->id}}" class="btn p-0">
+                        <small><i class="fa-solid fa-ban text-danger"><small class="d-block">キャンセル</small></i></small>
+                      </button>
+                      <x-modal-cancel-takeout :takeout-order-id="$takeoutOrder->id" :takeout-order-menu-name="$takeoutOrder->menu->name" />
+                    </div>
+                  </div>
                   @else
                     <i class="fa-solid fa-check text-danger"></i>
                   @endif
