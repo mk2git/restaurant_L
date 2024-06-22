4c7cc8296b8d79705d6f771a84582282b0000aad
mk2git <86880555+mk2git@users.noreply.github.com>
2024-06-22 15:19:21 +0900

    レスポンシブルデザインに修正

--- resources/views/checkouts/show.blade.php
+++ resources/views/checkouts/show.blade.php
@@ -4,18 +4,19 @@
     ['name' => 'テーブル会計', 'link' => route('checkout.index')],
     ['name' => $table->seat_type . '-' . $table->seat_number, 'link' => '']
   ]" />
-  <div class="container mt-5 w-75">
+  <div class="container my-5">
       @if ($errors->any())
          <x-error-message />
       @endif
       
-    <div class="container bg-light p-5">
-      <h3 class="text-center mb-4"><i class="fa-regular fa-credit-card"></i>&nbsp;&nbsp;お会計</h3>
-
-      <div class="bill bg-white w-50 mx-auto px-4 pt-5 pb-3 mb-5">
-        <p class="h4">テーブル：{{$table->name}}<small class="float-end h6 pt-3">{{$checkoutTime->created_at->format('Y-m-d H:i')}}</small></p>
+    <div class="container bg-light p-3 bill-outside">
+      <h3 class="text-center my-4"><i class="fa-regular fa-credit-card"></i>&nbsp;&nbsp;お会計</h3>
+      <div class="bill-inside bg-white px-4 pt-5 pb-3 mb-4">
+        <p class="border-bottom border-2 d-flex justify-content-between">
+          <span class="h4 ps-3">{{$table_name}}</span> 
+          <small class="pt-3">{{$checkoutTime->created_at->format('Y-m-d H:i')}}</small>
+        </p>
         
-        <hr>
         <x-show-bill :orders="$orders" />
 
         <p>支払い方法</p>
@@ -27,7 +28,7 @@
      
       <div class="text-center">
           <input type="hidden" name="table_id" value="{{$table->id}}">
-          <button type="submit" class="btn btn-success w-25">CheckOut</button>
+          <button type="submit" class="btn checkout-btn d-block mx-auto mb-2">CheckOut</button>
         </form>
       </div>
     </div>
