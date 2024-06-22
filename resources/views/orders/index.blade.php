4876ee83dc83d7ee26db1822f50f5845a8e369f5
mk2git <86880555+mk2git@users.noreply.github.com>
2024-06-23 00:02:43 +0900

    レスポンシブルデザインに修正

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -4,39 +4,42 @@
         ]"
     />
 
-    <div class="container w-75 mx-auto my-5">
-        <h3 class="text-center mb-5"><i class="fa-solid fa-chair"></i>&nbsp;&nbsp;どのテーブルの注文を受け付けますか？</h3>
-
-        <div class="container border bg-light p-5 mb-5">
-            <div class="row bg-white">
-                <div class="col-10 d-flex justify-content-between flex-wrap">
+    <h3 class="text-center my-5 table-title"><i class="fa-solid fa-chair"></i>&nbsp;&nbsp;どのテーブルの注文を受け付けますか？</h3>
+    
+    <div class="my-5 mx-0 mx-auto table-container">
+        <div class="bg-light p-2">
+            <div class="row bg-white m-2 mb-1">              
+                <div class="col-10 d-flex justify-content-start flex-wrap px-0">
                     @foreach ($tables as $table)
                         @if ($table->seat_type == 'A' && $table->status == config('table.未使用'))
-                            <x-Atable-style :seat-type="$table->seat_type" :seat-number="$table->seat_number" />
+                            <button class="btn p-0 btn-Atable">
+                               <x-Atable-style :seat-type="$table->seat_type" :seat-number="$table->seat_number" /> 
+                            </button>                          
                         @elseif($table->seat_type == 'A' && $table->status == config('table.使用中'))
-                        <button type="button" class="btn p-0 select-Atable" data-bs-toggle="modal" data-bs-target="#selectTableAModal{{$table->id}}" data-table-id="{{ $table->id }}">
-                             <x-Atable-style :seat-type="$table->seat_type" :seat-number="$table->seat_number" class="using-table" />
-                        </button>
-                        <x-modal-select-order-Atable :table-id="$table->id" :seat-type="$table->seat_type" :seat-number="$table->seat_number" />
+                            <button type="button" class="btn p-0 btn-Atable" data-bs-toggle="modal" data-bs-target="#selectTableAModal{{$table->id}}" data-table-id="{{ $table->id }}">
+                                <x-Atable-style :seat-type="$table->seat_type" :seat-number="$table->seat_number" class="using-table" />
+                            </button>
+                            <x-modal-select-order-Atable :table-id="$table->id" :seat-type="$table->seat_type" :seat-number="$table->seat_number" />
                         @endif
                     @endforeach
                 </div>
                 <div class="col-2">
                     <div class="mt-3">
-                        <i class="fa-solid fa-restroom h3"></i>
+                        <i class="fa-solid fa-restroom border-secondary border rounded p-1 p-md-2"></i>
                     </div>
                 </div>
             </div>
 
-            <hr class="w-75 m-0">
-
-            <div class="row bg-white">
-                <div class="col-10 d-flex justify-content-between flex-wrap">
+            <div class="row bg-white m-2 mt-0">
+                <div class="col-10 d-flex justify-content-between flex-wrap px-0">
                     @foreach ($tables as $table)
                         @if ($table->seat_type == 'B' && $table->status == config('table.未使用'))
-                             <x-Btable-style :seat-type="$table->seat_type" :seat-number="$table->seat_number" />
+                            <button class="btn p-0 btn-Btable">
+                                <x-Btable-style :seat-type="$table->seat_type" :seat-number="$table->seat_number" />
+                            </button>
+                            
                         @elseif($table->seat_type == 'B' && $table->status == config('table.使用中'))
-                            <button type="button" class="btn p-0 select-Btable" data-bs-toggle="modal" data-bs-target="#selectTableBModal{{$table->id}}" data-table-id="{{ $table->id }}">
+                            <button type="button" class="btn p-0 btn-Btable" data-bs-toggle="modal" data-bs-target="#selectTableBModal{{$table->id}}" data-table-id="{{ $table->id }}">
                                 <x-Btable-style :seat-type="$table->seat_type" :seat-number="$table->seat_number" class="using-table" />
                             </button>
                             <x-modal-select-order-Btable :table-id="$table->id" :seat-type="$table->seat_type" :seat-number="$table->seat_number" />
@@ -44,7 +47,7 @@
                     @endforeach
                 </div>
                 <div class="col-2 border d-flex align-items-center justify-content-center">
-                    <span>厨房</span>
+                    <span class="kitchen-size">厨房</span>
                 </div>
             </div>
         </div>
